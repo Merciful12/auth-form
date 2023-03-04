8ed9cb7f5f7832a782dd20974820d0f11d95c6c5
Stanislav Zhatikov <future.12@yandex.ru>
2023-03-04 19:52:42 +0400

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import linaria from '@linaria/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? process.env.SITE_BASE : '',
   plugins: [react(), linaria()],
 })
